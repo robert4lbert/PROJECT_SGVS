0d3ff1a677b8e68d72ca2fe38908e39aa44ae80f
Robert Albert <86208510+robert4lbert@users.noreply.github.com>
2021-08-23 21:06:38 -0300

    Update Project BLL

---
+++ SGVES.BLL/Autenticacao/LoginBLO.cs
@@ -0,0 +1,29 @@
+﻿
+
+using SGVE.BLL.CustomException;
+using SGVE.DAL;
+using SGVS.Entities.Classes;
+
+
+namespace SGVE.BLL.Autenticacao
+{
+    // B.L.O = Busines Library Owner
+    public class LoginBLO
+    {
+        private LoginDAO _usuarioDAO;
+        public TB001_LOGIN ObterUsuarioLogin(string userName, string password)
+        {
+            _usuarioDAO = new LoginDAO();
+            var usuario = _usuarioDAO.ObterUsuario(userName, password);
+
+            if (usuario != null)
+            {
+                return usuario;
+            }
+            else
+            {
+                throw new UsuarioNaoCadastradoException();
+            }
+        }
+    }
+}
---
+++ SGVES.BLL/CustomException/UsuarioNaoCadastradoException.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGVE.BLL.CustomException
+{
+    public class UsuarioNaoCadastradoException : Exception
+    {
+        public UsuarioNaoCadastradoException()
+        {
+
+        }
+
+        public UsuarioNaoCadastradoException(String message) : base(message)
+        {
+
+        }
+        
+        public UsuarioNaoCadastradoException(String message, Exception inner) : base(message, inner)
+        {
+            
+        }
+    }
+}
