95f22ec9458e81d31dc0c70757788c66f3eac23d
Robert Albert <86208510+robert4lbert@users.noreply.github.com>
2021-08-23 21:07:21 -0300

    Upload Project(DAL)

---
+++ SGVES.DAL/Conexao.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SGVE.DAL
+{
+    public class Conexao
+    {
+        public static string _ConnectionString = ConfigurationManager.ConnectionStrings["ConnectSQLSERVER"].ConnectionString;        
+        public static SqlConnection _sqlConnection = new SqlConnection(_ConnectionString);
+
+        public static void Connect()
+        {
+            if (_sqlConnection.State == ConnectionState.Closed)
+            {
+                _sqlConnection.Open();
+            }
+
+        }
+        public static void Disconnect()
+        {
+            if (_sqlConnection.State == ConnectionState.Open)
+            {
+                _sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ SGVES.DAL/LoginDAO.cs
@@ -0,0 +1,50 @@
+﻿using SGVS.Entities.Classes;
+
+
+using System;
+using System.Data.SqlClient;
+
+namespace SGVE.DAL
+{
+    // D.A.O = Data Adm Owner
+    public class LoginDAO
+    {
+        public TB001_LOGIN ObterUsuario(string userName, string password)
+        {
+            try
+            {
+                var command = new SqlCommand();
+                command.Connection = Conexao._sqlConnection;          
+                command.CommandText = "SELECT * FROM TB001_LOGIN " +
+                                               "WHERE TB001_S_USUARIO = @USUARIO " +
+                                               "AND TB001_S_PASSWORD = @SENHA";
+
+                command.Parameters.AddWithValue("@USUARIO", userName);
+                command.Parameters.AddWithValue("@SENHA", password);
+
+                Conexao.Connect();
+                var reader = command.ExecuteReader();
+
+                TB001_LOGIN LOGIN = null;
+
+                while (reader.Read())
+                {
+                    LOGIN = new TB001_LOGIN();
+                    LOGIN.Codigo = Convert.ToInt32(reader["TB001_I_COD"]);
+                    LOGIN.TB001_S_USUARIO = reader["TB001_S_USUARIO"].ToString();
+                    LOGIN.TB001_S_PASSWORD = reader["TB001_S_PASSWORD"].ToString();
+                    LOGIN.TB001_S_PROFILE = reader["TB001_S_PROFILE"].ToString();
+                }
+                return LOGIN;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexao.Disconnect();
+            }
+        }
+    }
+}
